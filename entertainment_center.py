095813f28f00b1d9a1bcb19e527eb36bb8a2a41d
ChinoALM <mortona35@gmail.com>
2017-11-29 18:31:56 -0500

    comments added to functions to pass requirements in media.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,40 +3,49 @@ import fresh_tomatoes
 import media
 
 avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
                      "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
                      "http://www.youtube.com/watch?v=-9ceBgWV8io")
 
 batman_begins = media.Movie("Batman Begins",
-                        "https://upload.wikimedia.org/wikipedia/en/a/af/Batman_Begins_Poster.jpg",
-                        "https://www.youtube.com/watch?v=vak9ZLfhGnQ")
+                    "Can Batman save Gotham from the Scarecrow that lurks in the shadows?",
+                    "https://upload.wikimedia.org/wikipedia/en/a/af/Batman_Begins_Poster.jpg",
+                    "https://www.youtube.com/watch?v=vak9ZLfhGnQ")
 
 jurassic_park = media.Movie("Jurassic Park",
-                "https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg",
-                "https://www.youtube.com/watch?v=lc0UehYemQA")
+                    "The most phenomenal discovery of our time becomes the greatest adventure of all time!",
+                    "https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg",
+                    "https://www.youtube.com/watch?v=lc0UehYemQA")
 
 blow = media.Movie("Blow",
-                "https://upload.wikimedia.org/wikipedia/en/b/bf/Blow_poster.jpg",
-                "https://www.youtube.com/watch?v=q8lGHQn_n9Y")
+                    "A tale of how the cocaine market was born the United States during the 1970s.",
+                    "https://upload.wikimedia.org/wikipedia/en/b/bf/Blow_poster.jpg",
+                    "https://www.youtube.com/watch?v=q8lGHQn_n9Y")
 
 edward_scissorhands = media.Movie("Edward Scissorhands",
-                     "https://upload.wikimedia.org/wikipedia/en/3/3b/Edwardscissorhandsposter.JPG",
-                     "https://www.youtube.com/watch?v=M94yyfWy-KI")
+                    "The most incredible tale of the most unusual creature!",
+                    "https://upload.wikimedia.org/wikipedia/en/3/3b/Edwardscissorhandsposter.JPG",
+                    "https://www.youtube.com/watch?v=M94yyfWy-KI")
 
 toy_story = media.Movie("Toy Story",
+                    "A story about a boy and his toys that magically come alive!",
                     "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                     "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 the_sand_lot = media.Movie("The Sandlot",
-                     "https://upload.wikimedia.org/wikipedia/en/d/d4/Sandlot_poster.jpg",
-                     "https://www.youtube.com/watch?v=-QDq-e1GbjE")
+                    "The neighborhood kids battle the Beast!",
+                    "https://upload.wikimedia.org/wikipedia/en/d/d4/Sandlot_poster.jpg",
+                    "https://www.youtube.com/watch?v=-QDq-e1GbjE")
 
 the_saint = media.Movie("The Saint",
-                     "https://upload.wikimedia.org/wikipedia/en/8/86/The_Saint_1997_poster.jpg",
-                     "https://www.youtube.com/watch?v=QWIIu9SWYC0")
+                    "A man with no face can not be indentified.",
+                    "https://upload.wikimedia.org/wikipedia/en/8/86/The_Saint_1997_poster.jpg",
+                    "https://www.youtube.com/watch?v=QWIIu9SWYC0")
 
 top_gun = media.Movie("Top Gun",
-                     "https://upload.wikimedia.org/wikipedia/en/4/46/Top_Gun_Movie.jpg",
-                     "https://www.youtube.com/watch?v=ioWpe3hdFH0")
+                    "Elite United States Navy pilots compete to be the best!",
+                    "https://upload.wikimedia.org/wikipedia/en/4/46/Top_Gun_Movie.jpg",
+                    "https://www.youtube.com/watch?v=ioWpe3hdFH0")
 
 movies = [avatar, batman_begins, jurassic_park, blow, edward_scissorhands, toy_story,
           the_sand_lot, the_saint, top_gun]
--- media.py
+++ media.py
@@ -5,13 +5,19 @@ import webbrowser
 class Movie():
 	"""This calss provides a way to store move related information"""
 	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-	# WHEN A LIST IS CONSTANT , MAKE THE VARIABLE NAME IN ALL CAPS
+	""" WHEN A LIST IS CONSTANT , MAKE THE VARIABLE NAME IN ALL CAPS"""
 
-	def __init__(self, movie_title, poster_image, trailer_youtube):
+	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+		"""Func contructs an instance of Movie class for each input passed through as strings of the
+		movie title, it's storyline, poster_image, and trailer to which the args are assigned to
+		corresponding instance variables"""
 		self.title = movie_title
-		#self.storyline = movie_storyline
+		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 
 	def show_trailer(self):
+		"""Func constructs an instance of Movie class for which self is passed through as input,
+		telling the browser to open the trailer to the corresponding YouTube video when the user
+		selects from your website, after your site has load properly."""
 		webbrowser.open(self.trailer_youtube_url)
